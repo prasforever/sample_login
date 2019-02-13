51e9724ebba9c5092bfc87adecc312a556b42aa0
prasforever <prasannapoojary@outlook.com>
2019-02-14 01:16:00 +0530

    adds unique usernames and emailonly

--- routes/skillsRoutes.js
+++ routes/skillsRoutes.js
@@ -3,9 +3,6 @@ const Skills = mongoose.model("skills");
 
 module.exports = app => {
   app.post("/api/getSkills", async (req, res) => {
-    console.log("In SkillsFunc");
-    console.log(req.body);
-
     const skills = await Skills.find(
       {
         value: { $regex: ".*" + req.body.value + ".*" }
