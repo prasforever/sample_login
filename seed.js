3920a1939bc873d9de1e0003a33771af400a8c49
prasforever <prasannapoojary@outlook.com>
2019-02-13 17:44:47 +0530

    added countries at server

---
+++ seed.js
@@ -0,0 +1,25 @@
+const { Seeder } = require("mongo-seeding");
+const path = require("path");
+
+const keys = require("./config/keys.js");
+
+const config = {
+  database: keys.mongoURI,
+  inputPath: path.resolve(__dirname, "./data"),
+  dropDatabase: true
+};
+
+const seeder = new Seeder(config);
+const collections = seeder.readCollectionsFromPath(path.resolve("./data"));
+const main = async () => {
+  try {
+    await seeder.import(collections);
+    console.log("Seed complete!");
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(0);
+  }
+};
+
+main();
